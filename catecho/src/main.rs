93f405cf0f105d8a1dccadf29df3524005b7158f
Vermee81 <hrksb5029@gmail.com>
2025-01-06 01:42:25 +0900

    :sparkles: テキスト引数を必須にする

--- catecho/src/main.rs
+++ catecho/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "Vermee81")]
+#[command(version, about = "An echo-like command adds cat meows")]
+struct Args {
+    #[arg(required = true, help = "Input text")]
+    text: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:#?}", args);
 }
