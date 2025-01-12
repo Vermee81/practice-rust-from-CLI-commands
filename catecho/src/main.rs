3b20a03dc8d4f5b20c89b1e9f6f3b0fcad6e0a02
Vermee81 <hrksb5029@gmail.com>
2025-01-13 00:31:41 +0900

    :sparkles: 改行の削除オプションを追加する

--- catecho/src/main.rs
+++ catecho/src/main.rs
@@ -6,10 +6,13 @@ use clap::Parser;
 struct Args {
     #[arg(required = true, help = "Input text")]
     text: Vec<String>,
+    #[arg(short = 'n', help = "Do not print newline")]
+    no_newline: bool,
 }
 
 fn main() {
     let args = Args::parse();
     let text = args.text;
-    println!("{}{}", text.join(" "), " MEOW");
+    let no_newline = args.no_newline;
+    print!("{}{}{}", text.join(" ")," MEOW",if no_newline {""} else {"\n"} );
 }
