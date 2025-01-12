e2e1273dc1d6cffab016bfbe29cb9ecee3bae83d
Vermee81 <hrksb5029@gmail.com>
2025-01-13 00:04:32 +0900

    :green_heart: 一番シンプルなテストを追加する

--- catecho/tests/cli.rs
+++ catecho/tests/cli.rs
@@ -1,6 +1,13 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+// 引数に文字列を指定して実行して、その文字列が標準出力に出力されることを確認
+#[test]
+fn runs() {
+    let mut cmd: Command = Command::cargo_bin("catecho").unwrap();
+    cmd.arg("Hello").assert().success().stdout("Hello\n");
+}
+
 // 引数なしで実行してUSAGEが標準エラーに出力されることを確認
 #[test]
 fn dies_no_args() {
